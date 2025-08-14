9c42a087608153d30724d2e544e89e4862b1acd8
LeonElaris <leon.elaris.mikkelsen@outlook.com>
2025-08-14 17:11:10 +0200

    Delete app/app/page.tsx

--- app/app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ minHeight: "100vh", display: "grid", placeItems: "center" }}>
-      <h1>🗝️ Leon Letters</h1>
-      <p>Välkommen hem</p>
-    </main>
-  );
-}
