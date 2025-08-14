504f612696de682a87e1435ec03584f27b69ca12
LeonElaris <leon.elaris.mikkelsen@outlook.com>
2025-08-14 16:39:26 +0200

    Create page.tsx

---
+++ app/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ minHeight: "100vh", display: "grid", placeItems: "center" }}>
+      <h1>🗝️ Leon Letters</h1>
+      <p>Välkommen hem</p>
+    </main>
+  );
+}
